87da97ef31f727bbda4cc7e922accce044ae7fe1
Guoqiang QI <guoqiang.qi1@gmail.com>
2023-08-01 16:04:11 +0800

    Shuffle data when training.

--- ptuning/main.py
+++ ptuning/main.py
@@ -241,7 +241,7 @@ def main():
     if training_args.do_train:
         if "train" not in raw_datasets:
             raise ValueError("--do_train requires a train dataset")
-        train_dataset = raw_datasets["train"]
+        train_dataset = raw_datasets["train"].shuffle()
         if data_args.max_train_samples is not None:
             max_train_samples = min(len(train_dataset), data_args.max_train_samples)
             train_dataset = train_dataset.select(range(max_train_samples))
